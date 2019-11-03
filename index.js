7e76cbaece5a3f74836da7e2a84e05036246928e
TATY <tatyccsilva@gmail.com>
2019-11-03 21:29:44 +0100

    Get beers from given json

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
 const cors = require('cors')
 const microbreweries = require('./microbreweries.json')
+const beers = require('./beers.json')
 
 const app = express()
 app.use(cors())
 
 app.get('/microbreweries', (req, res) => {
     res.send(microbreweries)
-});
+})
+
+app.get('/beers', (req, res) => {
+    res.send(beers)
+})
 
 const port = process.env.PORT || 4000
 app.listen(port, () => console.log(`Listening on :${port}`))
